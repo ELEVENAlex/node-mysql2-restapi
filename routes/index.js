92147085fe42906bd5cf64f687ee950b8cf604c3
Alex Manet <alexander.manap@gmail.com>
2023-02-01 13:57:45 +0100

    Añadidas las rutas del index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.send('Hola Mundo');
 });
 
+router.get('/employees', (req, res, next) => {
+  res.send('Obteniendo empleados');
+});
+
+router.post('/employees', (req, res, next) => {
+  res.send('Creando empleados');
+});
+
+router.put('/employees', (req, res, next) => {
+  res.send('Actualizando empleados');
+});
+
+router.delete('/employees', (req, res, next) => {
+  res.send('Eliminando empleados');
+});
 module.exports = router;
