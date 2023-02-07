7a773e4c5bb659b90f0c4501edeaf0fefa45ead5
Alex Manet <alexander.manap@gmail.com>
2023-02-07 10:19:15 +0100

    Controlador de errores en employees.controller

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -1,67 +1,95 @@
 const pool = require('../db.js')
 
 const getEmployees = async(req, res, next) => {
-    const [row] = await pool.query('SELECT * FROM employee')
-    res.json(row)
+    try{
+        const [row] = await pool.query('SELECT * FROM employee')
+        res.json(row)
+    } catch (error){
+        return res.status(500).json({
+            message: 'Something went wrong!'
+        })
+    }
 }
 
 const getEmployee = async(req, res, next) => {
+    try{
+        const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
 
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [req.params.id])
-
-    if(rows.length <= 0){
-        return res.status(404).json({
-            message: 'Employee not found!'
+        if(rows.length <= 0){
+            return res.status(404).json({
+                message: 'Employee not found!'
+            })
+        }
+        res.json(rows[0])
+    } catch (error){
+        return res.status(500).json({
+            message: 'Something went wrong!'
         })
     }
-    res.json(rows[0])
 }
 
 const createEmployee = async (req, res, next) => {
-    const name = req.body.name
-    const salary = req.body.salary
-    const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
-    //console.log(name, salary)
-    res.send({
-        id: rows.insertId,
-        name,
-        salary
-    });
+    try{
+        const name = req.body.name
+        const salary = req.body.salary
+        const [rows] = await pool.query('INSERT INTO employee (name, salary) VALUES (?, ?)', [name, salary])
+        //console.log(name, salary)
+        res.send({
+            id: rows.insertId,
+            name,
+            salary
+        })
+    } catch (error){
+        return res.status(500).json({
+            message: 'Something went wrong!'
+        })
+    }
 }
 
 const updateEmployee = async (req, res, next) => {
-
-    const {id} = req.params
-    const {name, salary} = req.body
+    try{
+        const {id} = req.params
+        const {name, salary} = req.body
+        
+        const [result] = await pool.query('UPDATE employee SET name = IFNULL(?,name), salary = IFNULL(?,salary) WHERE id = ?', [name, salary, id])
+        
+        if(result.affectedRows === 0){
+            return res.status(404).json({
+                message: 'Employee not found'
+            })
+        }
     
-    const [result] = await pool.query('UPDATE employee SET name = ?, salary = ? WHERE id = ?', [name, salary, id])
-    
-    if(result.affectedRows === 0){
-        return res.status(404).json({
-            message: 'Employee not found'
+        const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
+        res.json(rows[0])
+    } catch (error){
+        return res.status(500).json({
+            message: 'Something went wrong!'
         })
     }
-
-    const [rows] = await pool.query('SELECT * FROM employee WHERE id = ?', [id])
-    res.json(rows[0])
 }
 
 const deleteEmployee = async (req, res, next) => {
-    const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
+    try{
+        const [result] = await pool.query('DELETE FROM employee WHERE id = ?', [req.params.id])
 
-    if(result.affectedRows <= 0){
-        return res.status(404).json({
-            message: 'Employee not found!'
+        if(result.affectedRows <= 0){
+            return res.status(404).json({
+                message: 'Employee not found!'
+            })
+        }
+        res.send('Eliminado usurio')
+    
+        // if(rows.length <= 0){
+        //     return res.status(404).json({
+        //         message: 'Employee not found!'
+        //     })
+        // }
+        // res.json(rows[0])
+    } catch (error){
+        return res.status(500).json({
+            message: 'Something went wrong!'
         })
     }
-    res.send('Eliminado usurio')
-
-    // if(rows.length <= 0){
-    //     return res.status(404).json({
-    //         message: 'Employee not found!'
-    //     })
-    // }
-    // res.json(rows[0])
 }
 
 module.exports = {
